57fb949970ee588f368ebe5c961b261ae0b41e65
godcandidate <godcandidate101@gmail.com>
2025-08-13 15:50:39 +0000

    chore(package): remove packages

--- frontend/public/config.js
+++
@@ -1,5 +0,0 @@
-// This file will be available at /config.js in the browser
-window.APP_CONFIG = {
-  AUTH_API_URL: 'http://localhost:9193/api/v1',
-  TASK_API_URL: 'http://localhost:9191/api/v1'
-};
